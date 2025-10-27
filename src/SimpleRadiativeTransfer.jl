2f1ce61ddf8df55708d7936700a7b089a35e8aef
leo.demaine <leo.demaine@ipsl.fr>
2025-10-27 11:40:24 +0100

    Adding types

--- src/SimpleRadiativeTransfer.jl
+++ src/SimpleRadiativeTransfer.jl
@@ -6,4 +6,37 @@ end
 
 # Write your package code here.
 
+
+#==============================================================================#
+# Band Types
+
+abstract type Band end
+
+## Short waves
+struct SW <: Band
+    alpha
+    c_opacity
+end
+
+function sw_create(alpha = 1, c_opacity = 0.9)
+    return SW(alpha, c_opacity)
+end
+
+## Long waves
+struct LW <: Band
+    alpha
+    c_opacity
+end
+
+function lw_create(alpha = 0.5, c_opacity = 0.08)
+    return LW(alpha, c_opacity)
+end
+
+
+#==============================================================================#
+
+
+
+
+
 end
