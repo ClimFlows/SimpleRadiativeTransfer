b9c38dacc03352ebe79e86818e41a99137cafb81
leo.demaine <leo.demaine@ipsl.fr>
2025-10-29 16:24:23 +0100

    Cleaner version + UV band now available

--- src/SimpleRadiativeTransfer.jl
+++ src/SimpleRadiativeTransfer.jl
@@ -1,7 +1,7 @@
 module SimpleRadiativeTransfer
 
-export Band, SW, LW
-export sw_create, lw_create
+export Band, SW, LW, UV
+export sw_create, lw_create, uv_create
 
 export surface_temperature, stefan_bolt
 export τ, down_rad, up_rad, total_rad
@@ -13,141 +13,89 @@ export params_create
 
 abstract type Band end
 
-## Short waves
-struct SW <: Band
-    α
-    c_opacity
-end
 
-function sw_create(α = 1, c_opacity = 0.9)
-    return SW(α, c_opacity)
-end
-
-## Long waves
-struct LW <: Band
-    α
-    c_opacity
-end
-
-function lw_create(α = 1//2, c_opacity = 0.08)
-    return LW(α, c_opacity)
-end
 
 
 #==============================================================================#
 # Temperatures
 
-surface_temperature(params, net_flux) =
-    ( (net_flux/params.emissiv) / params.stefan )^(1//4)
+surface_temperature(net_flux, (; stefan, emissiv)) =
+    ( (net_flux/emissiv) / stefan )^(1//4)
 
 
-stefan_bolt(params, temp) = params.stefan * temp^4
+stefan_bolt(temp, (; stefan)) = stefan * temp^4
 
 #------------------------------------------------------------------------------#
 # Transmission function
 ## with pressure values
 
-function τ(params, band::Band, p_1, p_2, mu)
-    (; g, p_surf)    = params
+function τ end
+
+function τ(band::Band, p_1, p_2, μ)
     (; α, c_opacity) = band
-    return @. exp( - c_opacity/mu * abs( p_1^(1//α) - p_2^(1//α) )^α  )
-end
-
-function τ(params, band::SW, p_1, p_2, mu)
-    (; g, p_surf) = params
-    (; c_opacity) = band
-    return @. exp( -  c_opacity * (abs(p_1 - p_2))/mu )
-end
-
-function τ(params, band::LW, p_1, p_2, mu)
-    (; g, p_surf)    = params
-    (; α, c_opacity) = band
-    return @. exp( - c_opacity * sqrt( (abs(p_1^2 - p_2^2)) ))
+    return @. exp( - c_opacity/μ * abs( p_1^(1//α) - p_2^(1//α) )^α  )
 end
 
 
-τ(params, band::Band, p_1, p_2) = τ(params, band, p_1, p_2, 1)
+
+# When τ is called without μ parameter
+τ(band::Band, p_1, p_2) = τ(band, p_1, p_2, 1)
 
 
 #==============================================================================#
 # Fluxes functions
 
-function down_rad(params, band::Band, p_int)
-    (; mu, solarc) = params
-    return τ(params, band, p_int, 0, mu) * mu * solarc
+function down_rad end
+function up_rad end
+
+function down_rad!(flux, band::Band, p_int, source, μ)
+    flux .-= τ(band, p_int, 0, μ) * μ * source
 end
 
-function up_rad(params, band::Band, p_int, up_flux_surf)
-    (; p_surf) = params
-    return τ(params, band, p_surf, p_int) * up_flux_surf
+function up_rad!(flux, band::Band, p_int, source, μ)
+    flux .+= τ(band, p_int[1], p_int, μ) * source
 end
 
-function up_rad(params, band::SW, p_int, down_flux_surf)
-    (; p_surf, albedo) = params
-    up_flux_surf = down_flux_surf * albedo
-    return τ(params, band, p_surf, p_int, 3//5) * up_flux_surf
+function down_rad(band::Band, p_int, source, μ)
+    return τ(band, p_int, 0, μ) * μ * source
 end
 
-
-
-
-## Long wave
-
-function down_rad(params, band::LW, p_int, temp_col)
-    (; n_layers) = params
-    down_flux    = zeros(n_layers+1)
-
-    for i in 1:n_layers
-        for j in i:n_layers
-            down_flux[i] += stefan_bolt(params, temp_col[j]) *
-                ( τ(params, band, p_int[i], p_int[j]) -
-                 τ(params, band, p_int[i], p_int[j+1]) )
-        end
-    end
-
-    return down_flux
+function up_rad(band::Band, p_int, source, μ)
+    return τ(band, p_int[1], p_int, μ) * source
 end
 
 
-function up_rad(params, band::LW, p_int, temp_col, temp_surf, down_flux_surf)
-    (; p_surf, n_layers, emissiv) = params
+#==============================================================================#
+# Import
 
-    up_flux_surf = emissiv * stefan_bolt(params, temp_surf) +
-        (1 - emissiv) * down_flux_surf
+include("julia/longwave_band.jl")
+include("julia/shortwave_band.jl")
+include("julia/uv_band.jl")
 
-    up_flux = τ(params, band, p_surf, p_int) * up_flux_surf
 
-    for i in 2:n_layers+1
-        for j in 1:i-1
-            up_flux[i] += stefan_bolt(params, temp_col[j]) *
-                ( τ(params, band, p_int[i], p_int[j+1]) -
-                τ(params, band, p_int[i], p_int[j]) )
-        end
-    end
-
-    return up_flux
-
-end
-
-#------------------------------------------------------------------------------#
+#==============================================================================#
 ### Total flux
 
+
 function total_rad(params, band_sw::SW, band_lw::LW, p_int, temp_col)
-    down_rad_sw = down_rad(params, band_sw, p_int)
-    up_rad_sw   = up_rad(params, band_sw, p_int, down_rad_sw[1])
 
-    down_rad_lw = down_rad(params, band_lw, p_int, temp_col)
+    flux = net_rad(band_sw, p_int, params)
 
-    temp_surf   = surface_temperature(
-        params,
-        down_rad_lw[1] + down_rad_sw[1] - up_rad_sw[1]
-    )
+    flux_lw, temp_surf = net_rad(band_lw, p_int, temp_col, flux[1], params)
+    flux .+= flux_lw
 
-    up_rad_lw   = up_rad(
-        params, band_lw, p_int, temp_col, temp_surf, down_rad_lw[1]
-    )
+    return flux, temp_surf
+end
 
-    return up_rad_lw + up_rad_sw - down_rad_lw - down_rad_sw, temp_surf
+function total_rad(params, band_sw::SW, band_lw::LW, band_uv::UV, p_int, temp_col)
+
+    flux   = net_rad(band_sw, p_int, params)
+    flux .+= net_rad(band_uv, p_int)
+
+    flux_lw, temp_surf = net_rad(band_lw, p_int, temp_col, flux[1], params)
+    flux .+= flux_lw
+
+    return flux, temp_surf
 end
 
 #==============================================================================#
@@ -155,15 +103,13 @@ end
 
 
 function params_create(
-    n_layers = 30       ,
+    n_layers = 100      ,
     p_surf   = 101325.0 ,
-    mu       = 0.7      ,
-    solarc   = 1340/4   ,
     stefan   = 5.67e-8  ,
-    g        = 9.81     ,
     albedo   = 0.32     ,
     emissiv  = 0.9      ,
-    c_p      = 1005
+    c_p      = 1005     ,
+    g        = 9.81
     )
     """Create parameter set with default values"""
 
@@ -171,12 +117,10 @@ function params_create(
         n_layers = n_layers ,
         p_surf   = p_surf   ,
         albedo   = albedo   ,
-        mu       = mu       ,
-        solarc   = solarc   ,
         stefan   = stefan   ,
-        g        = g        ,
         emissiv  = emissiv  ,
-        c_p      = c_p
+        c_p      = c_p      ,
+        g        = g
     )
 
 end
---
+++ src/julia/longwave_band.jl
@@ -0,0 +1,104 @@
+#==============================================================================#
+# Definition of longwave band                                                  #
+#------------------------------------------------------------------------------#
+# Band Types
+
+struct LW <: Band
+    α
+    c_opacity
+end
+
+function lw_create(c_opacity, α = 1//2)
+    return LW(α, c_opacity)
+end
+
+
+#------------------------------------------------------------------------------#
+# Transmission function
+
+# More generic function in main, see wich one is easier
+# function τ(band::LW, p_1, p_2)
+#     (; α, c_opacity) = band
+#     return @. exp( - c_opacity * sqrt( (abs(p_1^2 - p_2^2)) ))
+# end# function τ
+
+
+#==============================================================================#
+# Fluxes functions
+
+function down_rad!(
+    flux,
+    band::LW,
+    p_int,
+    temp_col,
+    params
+    )
+
+    n_layers  = length(temp_col)
+
+    for i in 1:n_layers
+        for j in i:n_layers
+            flux[i] -= stefan_bolt(temp_col[j], params) *
+                ( τ(band, p_int[i], p_int[j]) -
+                τ(band, p_int[i], p_int[j+1]) )
+        end# for
+    end# for
+
+end# function down_rad
+
+
+
+function up_rad!(
+    flux,
+    band::LW,
+    p_int,
+    temp_col,
+    temp_surf,
+    up_flux_surf,
+    params
+    )
+
+    n_layers = length(temp_col)
+
+    flux    .+= τ(band, p_int[1], p_int) * up_flux_surf
+
+    for i in 2:n_layers+1
+        for j in 1:i-1
+            flux[i] += stefan_bolt(temp_col[j], params) *
+                ( τ(band, p_int[i], p_int[j+1]) -
+                τ(band, p_int[i], p_int[j]) )
+        end# for
+    end# for
+
+end# function up_rad
+
+
+function net_rad(
+    band::LW  ,
+    p_int     ,
+    temp_col  ,
+    surf_flux ,
+    params
+    )
+
+    (; emissiv) = params
+
+    flux = zeros(length(p_int))
+
+    down_rad!(flux, band, p_int, temp_col, params)
+
+    temp_surf = surface_temperature(
+        - surf_flux - flux[1],
+        params
+    )
+
+    up_flux_surf = emissiv * stefan_bolt(temp_surf, params) -
+        (1 - emissiv) * flux[1]
+
+
+    up_rad!(flux, band, p_int, temp_col, temp_surf, up_flux_surf, params)
+
+    return flux, temp_surf
+
+end# function net_rad
+
