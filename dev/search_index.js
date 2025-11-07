3c48263bf8072aeb140700a5c7c5247c55edcd78
Documenter.jl <documenter@juliadocs.github.io>
2025-11-07 10:22:18 +0000

    build based on fa68967

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SimpleRadiativeTransfer","page":"Home","title":"SimpleRadiativeTransfer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleRadiativeTransfer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
